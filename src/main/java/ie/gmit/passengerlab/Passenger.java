e91b4f67c64f519572fae5385a916d6403c7f99f
Vincent <vincent71hornak@gmail.com>
2022-02-09 09:58:44 +0000

    Title Feature Added

--- src/main/java/ie/gmit/passengerlab/Passenger.java
+++ src/main/java/ie/gmit/passengerlab/Passenger.java
@@ -1,4 +1,31 @@
 package ie.gmit.passengerlab;
 
-public class Passenger {
+public class Passenger
+{
+    private String Title;
+    private String Name;
+    private String ID;
+    private long Phone;
+    private int Age;
+
+    public Passenger(String Title, String Name, String ID, String Phone, int Age)
+    {
+        setTitle(Title);
+    }
+
+    private void setTitle(String Title)
+    {
+        if(Title == "Mr" || Title == "Mrs" || Title == "Ms")
+        {
+           this.Title = Title;
+        }
+        else
+        {
+            throw new IllegalArgumentException("Inavlid Title");
+        }
+    }
+    public String getTitle()
+    {
+        return Title;
+    }
 }
--- src/test/java/ie/gmit/passengerlab/PassengerTest.java
+++ src/test/java/ie/gmit/passengerlab/PassengerTest.java
@@ -1,4 +1,25 @@
 package ie.gmit.passengerlab;
 
-public class PassengerTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PassengerTest
+{
+    private Passenger myPass;
+
+    @Test
+    void testConstructorTitle()
+    {
+        myPass = new Passenger("Mr", "Conor", "1234567890", "09876543", 25);
+        assertEquals("Mr", myPass.getTitle());
+    }
+
+    @Test
+    void testConstructorInvalidTitle()
+    {
+        final String invalid = "Invalid Title";
+        Exception exceptionThrown = assertThrows(IllegalArgumentException.class, ()-> new Passenger("Miss", "Conor", "1234567890", "09876543", 25));
+        assertEquals(invalid, exceptionThrown.getMessage());
+    }
 }
